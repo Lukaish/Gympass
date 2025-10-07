5657fc3baffaf952e044f7aeadf2c0862badd891
Lukas Bernardi <lukassilvabernardi@outlook.com>
2025-10-06 22:51:40 -0300

    refactor(routes): create http/routes.ts and register /users endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
+import { appRoutes } from "./http/routes.js";
+;
 
 export const app = fastify();
-const prisma = new PrismaClient();
 
+app.register(appRoutes)
